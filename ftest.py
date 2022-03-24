b363787229b299f2e6b1dc8fc032d7a9e7b013c6
dong <dongvictorio@gmail.com>
2022-03-24 14:29:45 +0800

    initial commit for the project, nothing inportant for now.

---
+++ ftest.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import unittest
+
+class PageTest(unittest.TestCase):
+   def setUp(self):
+      self.browser = webdriver.Firefox()
+   #def tearDown(self):
+      #self.browser.quit()
+   def test_browser_title(self):
+      self.browser.get('http://localhost:8000')
+      self.assertIn('National ID System', self.browser.title)
+      self.fail('Finish the test!')
+if __name__ == '__main__':
+      unittest.main(warnings='ignore')
