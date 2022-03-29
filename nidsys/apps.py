de86aaa0c33158a932512c06f1447705fbaf535c
dong <dongvictorio@gmail.com>
2022-03-29 14:02:06 +0800

    unittest ok with 2 files added

---
+++ nidsys/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NidsysConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nidsys'
---
+++ nidsys/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def MainPage():
+   pass
