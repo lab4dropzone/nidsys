9f86bbaee5fe4325c77bd035599357a4dcd96fb2
dong <dongvictorio@gmail.com>
2022-04-13 17:11:10 +0800

    ok upto the first migration

--- nidsys/tests.py
+++ nidsys/tests.py
@@ -17,6 +17,34 @@ class HomePageTest(TestCase):
          'address': 'newAddress','contactno': 'newContactNo'})
       self.assertIn('newSurname', response.content.decode())
       self.assertTemplateUsed(response,'mainpage.html')
+      
+from nidsys.models import Registration
+class ORMTest(TestCase):
+   def test_saving_retrieving_list(self):
+      entry1 = Registration()
+      entry1.idno = '000001002345'
+      entry1.fname = 'Cyren Kate'
+      entry1.mname = 'Vie'
+      entry1.sname = 'De Belen'
+      entry1.address = 'Area 12 Dasmarinas Cavite '
+      entry1.contactno = '0987-1234567'
+      entry1.status = 'pending'
+      entry1.save()
+      entry2 = Registration()
+      entry2.idno = '000001002346'
+      entry2.fname = 'Acey Aljorie'
+      entry2.mname = 'Veral'
+      entry2.sname = 'Ponilas'
+      entry2.address = 'Area 35 Tagaytay Cavite '
+      entry2.contactno = '0912-4324567'
+      entry2.status = 'pending'
+      entry2.save()
+      items = Registration.objects.all()
+      self.assertEqual(items.count(), 2)
+      items1 = items[0]
+      items2 = items[1]
+      self.assertEqual(items1.fname, 'Cyren Kate')
+      self.assertEqual(items2.fname, 'Acey Aljorie')  
 
    """
    def test_root_url_resolves_to_mainpage_view(self):
